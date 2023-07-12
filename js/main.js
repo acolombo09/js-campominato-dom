e4dc60bbe6255990bcf920478f1a2506b061d506
acolombo09 <acolombo0911@gmail.com>
2023-07-12 16:48:28 +0200

    virtual_bombs_ok

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
 "use strict"
-/*
-Consegna
-Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
-(attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo 
-l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, 
-per evitare problemi con l’inizializzazione di git).
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà 
-prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata 
-al massimo una bomba, perciò nell’array delle bombe non potranno esserci due 
-numeri uguali.
-In seguito l’utente clicca su una cella: se il numero è presente nella lista 
-dei numeri generati - abbiamo calpestato una bomba - la cella si colora 
-di rosso e la partita termina. Altrimenti la cella cliccata si colora 
-di azzurro e l’utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o quando raggiunge 
-il numero massimo possibile di numeri consentiti 
-(ovvero quando ha rivelato tutte le celle che non sono bombe).
-Al termine della partita il software deve comunicare il punteggio, 
-cioè il numero di volte che l’utente ha cliccato su una cella 
-che non era una bomba.
----------------------------------------------------------------------------------------------------------
-BONUS:
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-----------------------------------------------------------------------------------------------------------
-Consigli del giorno:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, 
-dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo 
-ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
-*/
 
 // Creo una griglia quadrata di 10 quadrati per riga per 10 righe,
 // per un totale di 100 quadrati.
@@ -143,3 +107,64 @@ function onBtnClick() {
 // posso resettare prima dell'append che mi aggiunge i div
 // con un container.innerHTML = ""; (vuoto)
 // aggiungo un event listener per cambiare sfondo al click
+
+/*
+Consegna
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
+(attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo 
+l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, 
+per evitare problemi con l’inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà 
+prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata 
+al massimo una bomba, perciò nell’array delle bombe non potranno esserci due 
+numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista 
+dei numeri generati - abbiamo calpestato una bomba - la cella si colora 
+di rosso e la partita termina. Altrimenti la cella cliccata si colora 
+di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge 
+il numero massimo possibile di numeri consentiti 
+(ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, 
+cioè il numero di volte che l’utente ha cliccato su una cella 
+che non era una bomba.
+---------------------------------------------------------------------------------------------------------
+BONUS:
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+----------------------------------------------------------------------------------------------------------
+Consigli del giorno:
+Scriviamo prima cosa vogliamo fare passo passo in italiano, 
+dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo 
+ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un secondo momento.
+*/
+
+/*
+Genero 16 numeri casuali nello stesso range della difficoltà prescelta
+nella stessa cella può essere posizionata al massimo una bomba, 
+perciò nell’array delle bombe non potranno esserci due 
+numeri uguali.
+*/
+function bombsGenerator(){
+  let numbers = [];
+  while (numbers.length < 16) {
+    const bombsNumber = Math.floor(Math.random() * 100) + 1;//Genera un numero casuale tra 1 e 100
+    if (numbers.indexOf(bombsNumber) === -1) { //Verifico se il numero casuale non è già stato generato
+      numbers.push(bombsNumber); //Aggiunge il numero casuale all'array
+    }
+  }
+  return numbers;
+}
+// verifico se effettivamente funziona
+const bombs = bombsGenerator();
+for (let i = 0; i < bombs.length; i++) {
+  console.log(bombs[i]);
+  // tienilo da parte per stampare i numeri in un elemento HTML:
+  // document.getElementById('idElementoHTML').innerHTML += bombs[i];
+}
